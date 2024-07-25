fbfca12499acf04cb036c5707d74c35175a7a59e
pmdev <patriciamagodev@gmail.com>
2024-07-25 12:53:13 -0400

    respuestas agregadas 07-strings-exercises

--- Basic/07-strings-exercises.js
+++ Basic/07-strings-exercises.js
@@ -5,21 +5,43 @@ https://www.twitch.tv/videos/2200149072?t=00h08m02s
 */
 
 // 1. Concatena dos cadenas de texto
+let petName = "Miko "
+let petBreed= petName + "is a Cat"
+console.log(petBreed)
 
 // 2. Muestra la longitud de una cadena de texto
+console.log(petBreed.length)
 
 // 3. Muestra el primer y último carácter de un string
+console.log(petBreed[0])
+console.log(petBreed[12])
 
 // 4. Convierte a mayúsculas y minúsculas un string
+console.log(petBreed.toUpperCase())
+console.log(petBreed.toLocaleLowerCase())
 
 // 5. Crea una cadena de texto en varias líneas
+let miVariable = `Esta es una
+Cadena de Texto
+en Varias lineas`
+console.log(miVariable)
 
 // 6. Interpola el valor de una variable en un string
+let petOwner = "Patricia"
+console.log(`Hola, ${petOwner}! Tu mascota ${petName}ya está lista!`)
 
 // 7. Reemplaza todos los espacios en blanco de un string por guiones
+let miCadena = "Hola a Todos los Usuarios";
+let nuevaCadena = miCadena.replace(/ /g, "-"); // Implementando una Expresión Regular de tipo Global
+console.log(nuevaCadena)
 
 // 8. Comprueba si una cadena de texto contiene una palabra concreta
+console.log(nuevaCadena.includes("Usuarios"))
 
 // 9. Comprueba si dos strings son iguales
+let variableUno = "HOLA"
+let variablesDos = "HOLA"
+console.log(variableUno === variablesDos)
 
 // 10. Comprueba si dos strings tienen la misma longitud
+console.log(variableUno.length === variablesDos.length)
---
+++ Basic/08-conditionals.js
@@ -0,0 +1,34 @@
+// if, else if, else
+
+// if (si)
+
+let age = 18
+
+if (age == 37) {
+    console.log("La edad es 37")
+}
+
+// else (si no)
+
+if (age == 37) {
+    console.log("La edad es 37")
+} else {
+    console.log("La edad no es 37")
+}
+
+// else if (si no, si)
+
+if (age == 37) {
+    console.log("La edad es 37")
+} else if (age < 18) {
+    console.log("Es menor de edad")
+} else {
+    console.log("La edad no es 37 ni es menor de edad")
+}
+
+// Operador ternario
+const message = age == 37 ? "La edad es 37" : "La edad no es 37"
+console.log(message)
+
+// switch
+
