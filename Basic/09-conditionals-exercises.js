f5f9521e92714f4df00a6fa04df457fd768584a3
pmdev <patriciamagodev@gmail.com>
2024-08-05 18:15:12 -0400

    Respuestas agregadas 09-conditionals-exercises

--- Basic/09-conditionals-exercises.js
+++ Basic/09-conditionals-exercises.js
@@ -49,58 +49,106 @@ const adulto = edad == 18 ? "Ya eres un Adulto" : "Eres menor de Edad"
 console.log(adulto)
 
 // 6. Muestra en que estación del año nos encontramos dependiendo del valor de una variable "mes"
-let mes = 4; // Julio
+let season = "julio"
 
-if (mes === 3 || mes == 4 || mes === 5) {
-  console.log("Estamos en Primavera");
-} else if (mes >= 6 && mes <= 8) {
-  console.log("Estamos en Verano");
-} else if (mes >= 9 && mes <= 11) {
-  console.log("Estamos en Otoño");
-} else if (mes === 12 || mes === 1 || mes === 2) {
-  console.log("Estamos en Invierno");
-} else {
-  console.log("Mes inválido");
+if (season == "diciembre" || season == "enero" || season == "febrero") {
+    console.log("Estamos en Invierno")
+} else if (season == "marzo" || season == "abril" || season == "mayo") {
+    console.log ("Estamos en Primavera")
+} else if (season == "junio" || season == "julio" || season == "agosto") {
+    console.log ("Estamos en Verano")
+} else if (season == "septiembre" || season == "octubre" || season == "noviembre") {
+    console.log ("Estamos en Otoño")
+}
+else {
+    console.log("Mes no es valido, intentalo de nuevo")
 }
 
 // 7. Muestra el número de días que tiene un mes dependiendo de la variable del ejercicio anterior
+if (season == "enero" || season == "marzo" || season == "mayo" || season == "julio" || season == "agosto" || season == "octubre" || season == "diciembre") {
+    console.log("Este mes tiene 31 dias")
+} else if (season == "abril" || season == "junio" || season == "septiembre" || season == "noviembre") {
+    console.log ("Este mes tiene 30 dias")
+}
+else {
+    console.log ("Febrero tiene 28 dias o 29 dias cuando es año bisiesto")
+}
 
 // switch
 
 // 8. Usa un switch para imprimir un mensaje de saludo diferente dependiendo del idioma
+let idioma = "italiano"
+
+switch (idioma) {
+    case "español":
+        console.log("¡Buenos días!");
+        break;
+    case "inglés":
+        console.log("Good morning!");
+        break;
+    case "francés":
+        console.log("Bonjour!");
+        break;
+    case "italiano":
+        console.log("Buongiorno!");
+        break;
+    case "alemán":
+        console.log("Guten Morgen!");
+        break;
+    default:
+        console.log("Idioma no reconocido.");
+}
 
 // 9. Usa un switch para hacer de nuevo el ejercicio 6
+let mes = 8;
+
+switch (mes) {
+    case 12:
+    case 1:
+    case 2:
+        console.log("Estamos en invierno.");
+        break;
+    case 3:
+    case 4:
+    case 5:
+        console.log("Estamos en primavera.");
+        break;
+    case 6:
+    case 7:
+    case 8:
+        console.log("Estamos en verano.");
+        break;
+    case 9:
+    case 10:
+    case 11:
+        console.log("Estamos en otoño.");
+        break;
+    default:
+        console.log("Ingresa un número entre 1 y 12");
+}
 
 // 10. Usa un switch para hacer de nuevo el ejercicio 7
+let month = "febrero"; 
 
-let season = "noviembre"
-
-if (season == "enero") {
-    console.log("Estamos en Invierno")
-} 
-    else if (season == "febrero") {
-        console.log ("Estamos en Invierno")
-    } else if (season == "marzo") {
-        console.log ("Estamos en Primavera")
-    } else if (season == "abril") {
-        console.log ("Estamos en Primavera")
-    } else if (season == "mayo") {
-        console.log ("Estamos en Primavera")
-    } else if (season == "junio") {
-        console.log ("Estamos en Verano")
-    } else if (season == "julio") {
-        console.log ("Estamos en Verano")
-    } else if (season == "agosto") {
-        console.log ("Estamos en Verano")
-    } else if (season == "septiembre") {
-        console.log ("Estamos en Otoño")
-    } else if (season == "octubre") {
-        console.log ("Estamos en Otoño")
-    } else if (season == "noviembre") {
-        console.log ("Estamos en Otoño")
-    } else if (season == "diciembre") {
-        console.log ("Estamos en Invierno")
-    } 
-else {
-    console.log("Mes no es valido, intentalo de nuevo")
+switch (month) {
+    case "enero":
+    case "marzo":
+    case "mayo":
+    case "julio":
+    case "agosto":
+    case "octubre":
+    case "diciembre":
+        console.log("Este mes tiene 31 días.");
+        break;
+    case "abril":
+    case "junio":
+    case "septiembre":
+    case "noviembre":
+        console.log("Este mes tiene 30 días.");
+        break;
+    case "febrero":
+        console.log("Febrero tiene 28 días, excepto en años bisiestos.");
+        break;
+    default:
+        console.log("Mes inválido.");
 }
